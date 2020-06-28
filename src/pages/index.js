42171c4380998a7b22b36ec7a47c42b4844fbdcd
Engga <17691328+xprimen@users.noreply.github.com>
2020-06-28 15:01:42 +0700

    initialize

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link as Reachlink } from "@reach/router"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
@@ -14,8 +14,8 @@ const IndexPage = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <Reachlink to="/page-2/">Go to page 2</Reachlink> <br />
+    <Reachlink to="/using-typescript/">Go to "Using TypeScript"</Reachlink>
   </Layout>
 )
 
